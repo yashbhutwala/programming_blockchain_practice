60bf5bc8a15fe0afac08ee3099667e7642fa050d
Yash Bhutwala <>
2018-01-27 19:15:19 -0500

    session 1: ecc and helper

---
+++ helper.py
@@ -0,0 +1,26 @@
+from unittest import TestCase, TestSuite, TextTestRunner
+
+
+def run_test(test):
+    suite = TestSuite()
+    suite.addTest(test)
+    TextTestRunner().run(suite)
+
+
+def bytes_to_str(b, encoding='ascii'):
+    '''Returns a string version of the bytes'''
+    return b.decode(encoding)
+
+
+def str_to_bytes(s, encoding='ascii'):
+    '''Returns a bytes version of the string'''
+    return s.encode(encoding)
+
+
+class HelperTest(TestCase):
+
+    def test_bytes(self):
+        b = b'hello world'
+        s = 'hello world'
+        self.assertEqual(b, str_to_bytes(s))
+        self.assertEqual(s, bytes_to_str(b))
